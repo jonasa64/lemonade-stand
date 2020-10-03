87fce24047634d11a984508148a9f28d48ddafee
jonasa64 <hvidovrefifa@gmail.com>
2020-10-03 14:12:10 +0200

    set up roting

---
+++ lemonad-api/lemonade-react/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <p>We are experts in making quality lemonade. We are only useing the best lemons.</p>
+            <p>We have 5 years experience in making lemonade and served a lot of satisfied costumers</p>
+        </div>
+    )
+}
+
+export default About;
